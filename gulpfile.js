8c7441fcce5bab03f67446992697dfd1ac0f0056
yoji.higoshi <h33je5sg@gmail.com>
2019-10-01 18:01:30 +0900

    minifyの対応等
     * npm run buildからgulp defaultの挙動を変更
     * gulp build系の挙動を変更
     * gulp minify系の対応

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
-const {src, dest, parallel} = require('gulp');
+const {src, dest, parallel, series} = require('gulp');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
+const rename = require('gulp-rename');
 const cleanCss = require('gulp-clean-css');
 
-const buildJs = () => src('src/**/*.js')
+const buildJs = () => src('./src/**/*.js')
   .pipe(babel())
+  .pipe(dest('./dist/'));
+
+const minifyJs = () => src(['./dist/**/*.js', '!./dist/**/*.min.js'])
   .pipe(terser({
     mangle: false,
   }))
-  .pipe(dest('dist'));
+  .pipe(rename({extname: '.min.js'}))
+  .pipe(dest('./dist/'));
 
-const buildCss = () => src('src/**/*.css')
-  .pipe(cleanCss())
-  .pipe(dest('dist'));
+const buildCss = () => src('./src/**/*.css')
+  .pipe(dest('./dist/'));
+
+const minifyCss = () => src(['./dist/**/*.css', '!./dist/**/*.min.css'])
+  .pipe(rename({extname: '.min.css'}))
+  .pipe(dest('./dist/'));
 
 module.exports = {
   buildJs,
   buildCss,
-  default: parallel(buildJs, buildCss),
+  build: parallel(buildJs, buildCss),
+  minifyJs,
+  minifyCss,
+  minify: parallel(minifyCss, minifyJs),
+  default: series(parallel(buildJs, buildCss), parallel(minifyCss, minifyJs)),
 };
