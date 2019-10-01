a0dd89b6d68ae25040ca05e100c881d170923221
yoji.higoshi <h33je5sg@gmail.com>
2019-10-01 17:40:33 +0900

    gulp-terser

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const {src, dest, parallel} = require('gulp');
 const babel = require('gulp-babel');
+const terser = require('gulp-terser');
 const cleanCss = require('gulp-clean-css');
 
 const buildJs = () => src('src/**/*.js')
   .pipe(babel())
+  .pipe(terser({
+    mangle: false,
+  }))
   .pipe(dest('dist'));
 
 const buildCss = () => src('src/**/*.css')
